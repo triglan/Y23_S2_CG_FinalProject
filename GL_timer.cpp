8aa0a623a6ea9107f7da628d894bfb30145c470f
triglan <glanb4277@gmail.com>
2023-11-22 12:09:29 +0900

    template

---
+++ GL_timer.cpp
@@ -0,0 +1,8 @@
+﻿// 타이머
+#include "gl_func.h"
+
+void timerOperation(int value) {
+
+	glutTimerFunc(10, timerOperation, 1);
+	glutPostRedisplay();
+}
---
+++ gl_func.h
@@ -0,0 +1,10 @@
+﻿// OpenGL 기능 함수는 여기에 정의
+#ifndef GL_FUNC_H
+#define GL_FUNC_H
+#include "config.h"
+
+void keyDown(unsigned char KEY, int x, int y);
+void keyUp(unsigned char KEY, int x, int y);
+void timerOperation(int value);
+
+#endif
